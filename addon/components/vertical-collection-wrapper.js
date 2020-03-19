9ecbb3e8bba855cbf66a75a5d95ca01ce2f3a151
abadri <abadri@linkedin.com>
2020-03-18 19:36:45 -0700

    first commit

---
+++ addon/components/vertical-collection-wrapper.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../templates/components/vertical-collection-wrapper';
+
+export default Component.extend({
+  layout
+});
---
+++ app/components/vertical-collection-wrapper.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-vcollection/components/vertical-collection-wrapper';
